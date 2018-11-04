dcb68a8cd06c8ab9fa0a8029f3d70b0d357d5583
Jerry Pedersen <jerry.ck.pedersen@gmail.com>
2018-11-05 00:37:45 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Hello, world24</div>;
+  }
+}
